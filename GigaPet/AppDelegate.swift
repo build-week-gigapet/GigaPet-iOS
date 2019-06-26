aae3dec80e29fc65518ea8797acb0f7397ff2093
Jake Connerly <jlconnerly@gmail.com>
2019-06-25 22:42:02 -0500

    Added storyboard functionality with supporting files. Build loginChecker to verify usersign in

--- GigaPet/AppDelegate.swift
+++ GigaPet/AppDelegate.swift
@@ -15,12 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
-        let navigationController = UINavigationController()
-        let loginViewController = LoginViewController()
-        navigationController.pushViewController(loginViewController, animated: false)
-        window!.rootViewController = navigationController
-        window!.makeKeyAndVisible()
         return true
     }
 
